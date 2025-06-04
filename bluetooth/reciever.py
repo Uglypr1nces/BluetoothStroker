01d7fb6682c6a1c9f08ee36435961745789b403f
terrysgoodboy <terrysgoodboy@proton.me>
2025-06-04 04:39:31 -0500

    Update reciever.py
    not permanent, will be tweaked and made cleaner and better. this is for the rasberry PI u run this and it will allow RCFCOMM connections, when i get home from school today ima fix all of it

--- bluetooth/reciever.py
+++ bluetooth/reciever.py
@@ -1,15 +1,34 @@
-pyserver_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-server_sock.bind(("", bluetooth.PORT_ANY))
+import bluetooth
+
+# Create a Bluetooth RFCOMM server socket
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+server_sock.bind(("", 1))  # Bind explicitly to RFCOMM channel 1
 server_sock.listen(1)
 
-port = server_sock.getsockname()[1]
-print(f"Waiting for connection on RFCOMM channel {port}")
+print("🔌 Waiting for Bluetooth connection on RFCOMM channel 1...")
 
-client_sock, address = server_sock.accept()
-print(f"Accepted connection from {address}")
+# Make the service discoverable
+bluetooth.advertise_service(
+    server_sock,
+    "RPiBTServer",  # Service name
+    service_id="00001101-0000-1000-8000-00805F9B34FB",  # Standard SPP UUID
+    service_classes=["00001101-0000-1000-8000-00805F9B34FB", bluetooth.SERIAL_PORT_CLASS],
+    profiles=[bluetooth.SERIAL_PORT_PROFILE]
+)
 
-data = client_sock.recv(1024)
-print("Received:", data.decode())
+# Accept a connection
+client_sock, client_info = server_sock.accept()
+print(f" Accepted connection from {client_info}")
 
+# Receive a message
+try:
+    data = client_sock.recv(1024)
+    if data:
+        print("📥 Received:", data.decode())
+except Exception as e:
+    print(f"❌ Error receiving data: {e}")
+
+# Clean up
 client_sock.close()
 server_sock.close()
+print(" Server closed.")
